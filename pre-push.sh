e530f61c3ca9bf8d81fa12934c676f4872199129
Extigy <george.stagg@ncl.ac.uk>
2015-05-12 17:31:13 +0100

    fix the stats hooks

--- pre-push.sh
+++ pre-push.sh
@@ -2,14 +2,14 @@
 
 remote="$1"
 url="$2"
-
+root=$(git rev-parse --show-toplevel)
 scp teggers@teggers.eu:public_html/index.php ./
-sed -i "s/<p>Number of times compiled: <b>[0-9]\+<\/b><\/p>/<p>Number of times compiled: <b>$(cat ../../stats/compiled)<\/b><\/p>/g" index.php
-sed -i "s/<p>Number of pages: <b>[0-9]\+<\/b><\/p>/<p>Number of pages: <b>$(cat ../../stats/pages)<\/b><\/p>/g" index.php
-sed -i "s/<p>Number of words: <b>[0-9]\+<\/b><\/p>/<p>Number of words: <b>$(cat ../../stats/words)<\/b><\/p>/g" index.php
-sed -i "s/<p>Number of figures: <b>[0-9]\+<\/b><\/p>/<p>Number of figures: <b>$(cat ../../stats/figures)<\/b><\/p>/g" index.php
-sed -i "s/<p>Number of git commits: <b>[0-9]\+<\/b><\/p>/<p>Number of git commits: <b>$(cat ../../stats/commits)<\/b><\/p>/g" index.php
-sed -i "s/<p>Last commit message: <b>.\+<\/b><\/p>/<p>Last commit message: <b>$(cat ../../stats/lastmessage)<\/b><\/p>/g" index.php
+sed -i "s/<p>Number of times compiled: <b>[0-9]\+<\/b><\/p>/<p>Number of times compiled: <b>$(cat $root/stats/compiled)<\/b><\/p>/g" index.php
+sed -i "s/<p>Number of pages: <b>[0-9]\+<\/b><\/p>/<p>Number of pages: <b>$(cat $root/stats/pages)<\/b><\/p>/g" index.php
+sed -i "s/<p>Number of words: <b>[0-9]\+<\/b><\/p>/<p>Number of words: <b>$(cat $root/stats/words)<\/b><\/p>/g" index.php
+sed -i "s/<p>Number of figures: <b>[0-9]\+<\/b><\/p>/<p>Number of figures: <b>$(cat $root/stats/figures)<\/b><\/p>/g" index.php
+sed -i "s/<p>Number of git commits: <b>[0-9]\+<\/b><\/p>/<p>Number of git commits: <b>$(cat $root/stats/commits)<\/b><\/p>/g" index.php
+sed -i "s/<p>Last commit message: <b>.\+<\/b><\/p>/<p>Last commit message: <b>$(cat $root/stats/lastmessage)<\/b><\/p>/g" index.php
 scp index.php teggers@teggers.eu:public_html/
 rm ./index.php
 
